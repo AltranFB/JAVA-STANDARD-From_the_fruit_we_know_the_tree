f4c32c2e885253be734834ab62a9f83957742a5d
Grokwik <frederic@bodin.site>
2017-01-09 13:48:06 +0100

    Skeleton

---
+++ src/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+	
+	public static int DoIt() {
+		// -----------------------
+		//
+		//  INSERT YOUR CODE HERE
+		//
+		// -----------------------
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		// -----
+	}
+}
---
+++ src/TestCases.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestCases {
+	@Test
+		assertEquals("must return 1", 1, Solution.DoIt());
+	}
+
+	@Test
+    public void Test2() {
+    }
+
+	@Test
+	public void Test3() {
+    }
+
+	@Test
+	public void Test4() {
+    }
+
+	@Test
+	public void Test5() {
+    }
+
+	@Test
+	public void Test6() {
+    }
+}
